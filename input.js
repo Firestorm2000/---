50eb05a5473be3723313665ac7a58beb824267f7
Firestorm2000 <teenwolf2000@abv.bg>
2018-12-04 16:11:17 +0200

    Add files via upload

---
+++ input.js
@@ -0,0 +1,18 @@
+regExps = {
+"exercise_1": /[A-Z][a-z]+/,
+"exercise_2": /088[1-9]{7}/,
+"exercise_3": /[^01]+/,
+"exercise_4": /^[^\._0-9][a-zA-Z0-9?\.?_?]{2,17}$/,
+"exercise_5": /[19][^1]+$/,
+"exercise_6": /class=(\"|\')(.*)(\"|\')/
+};
+cssSelectors = {
+"exercise_1": "css",
+"exercise_2": "css",
+"exercise_3": "css",
+"exercise_4": "css",
+"exercise_5": "css",
+"exercise_6": "css",
+"exercise_7": "css",
+"exercise_8": "css"
+};
